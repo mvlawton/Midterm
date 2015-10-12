7e81efcb8d583a03554c903bda76084e79165cb3
Matthew Lawton <mlawton@udel.edu>
2015-10-12 04:02:46 -0400

    initial commit

---
+++ MidtermStarterProject/src/main/java/com/cisc181/core/Course.java
@@ -0,0 +1,13 @@
+package com.cisc181.core;
+
+import java.util.UUID;
+
+import com.cisc181.eNums.eMajor;
+
+public class Course {
+	private UUID CourseID;
+	private String CourseName;
+	private int GradePoints;
+	private eMajor Major;
+
+}
---
+++ MidtermStarterProject/src/main/java/com/cisc181/core/Enrollment.java
@@ -0,0 +1,23 @@
+package com.cisc181.core;
+
+import java.util.UUID;
+
+public class Enrollment {
+	private UUID SectionID;
+	private UUID StudentID;
+	private UUID EnrollmentID;
+	private double Grade;
+	
+	private Enrollment() {}
+	
+	public Enrollment(UUID SectionID, UUID SudentID) {
+		this.SectionID = SectionID;
+		this.StudentID = StudentID;
+		this.EnrollmentID = UUID.randomUUID();
+	}
+	
+	public void setGrade(double Grade) {
+		this.Grade = Grade;
+	}
+
+}
